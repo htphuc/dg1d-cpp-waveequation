cd2dac3489cc2d60912d6961b532f26a338fc569
Steven Dorsher <sdorsh1@lsu.edu>
2016-01-07 19:25:48 -0600

    added matmul method (tested) and a few lines of characteristic flux routine (untested)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 #include "ReferenceElement.h"
 using namespace std;
 
+vector<double> matmul(vector<double>& A, vector<double>& B, int dimA1,
+		       int dimA2B1, int dimB2);
+
+void rightHandSide(vector<double>& thegrid, vector<double>& uh,
+		   vector<double>& rhs, int order, int numelem, int pdenum,
+		   double speed);
+
 int main()
 {
   //  double lowlim =-20.;
@@ -92,17 +99,17 @@ void rightHandSide(vector<double>& thegrid, vector<double>& uh,
 
   //debug node and elemcount section NEXT TIME. WORKING on Char Flux. Should
   // be zero in first sub time step.and zeroth time step. HERE.
-  vector<double> lambda(4,0.0);
+  vector<double> lambda((pdenum-1)*(pdenum-1),0.0);
   lambda[0]=-speed;
   lambda[3]=speed;
 
-  vector<double> smatrix(4);
+  vector<double> smatrix((pdenum-1)*(pdenum-1));
   smatrix[0]=speed;
   smatrix[1]=-speed;
   smatrix[2]=1.0;
   smatrix[3]=1.0;
 
-  vector<double> sinv(4);
+  vector<double> sinv((pdenum-1)*(pdenum-1));
   sinv[0]=0.5/speed;
   sinv[1]=0.5;
   sinv[2]=-0.5/speed;
@@ -111,6 +118,9 @@ void rightHandSide(vector<double>& thegrid, vector<double>& uh,
   int pdebase = numelem*(order+1);
   int indL = order;
   int indR = 0;
+  vector<double> nx(2);
+  nx[0]=-1.0;
+  nx[1]=1.0;
   for(int elemnum=0; elemnum<numelem; elemnum++){
 
     int elemcount = elemnum*(order+1);
@@ -136,4 +146,43 @@ void rightHandSide(vector<double>& thegrid, vector<double>& uh,
     uext[2]=uh[2*pdebase+elemcount+indL];
     uext[3]=uh[2*pdebase+elemcount+indR];
 
-    vector<double> lambdaplus(4,0.0);
+    vector<double> nflux(2*(pdenum-1));
+
+    //what does 2 mean in the summation index? is it the pdenum-1 or is it
+    //the direction? or does the characteristic flux only work for
+    //waves propogating in two directions with opposite sign speeds? 
+    for(int j=0; j<2; j++){
+      vector<double> lambdaplus(2*(pdenum-1));
+      vector<double> lambdaminus(2*(pdenum-1));
+      for(int k=0; k<2; k++){
+	if(nx[j] * lambda[2 * j + k] < 0.0) {
+	  lambdaminus[2 * k + k] = nx[j] * lambda[2 * j + k];
+	}else{
+	  lambdaplus[2 * k + k] = nx[j] * lambda[2 * j + k];
+	}
+      }
+    }
+  }
+  vector<double> ident = matmul(smatrix,sinv,2,2,2);
+  for(int i=0; i<4; i++){
+    cout << ident[i] << " ";
+  }
+  cout << endl;
+}
+
+vector<double> matmul(vector<double>& A, vector<double>& B, int dimA1,
+		       int dimA2B1, int dimB2){
+  vector<double> C(dimA1*dimB2);
+  
+  for(int i=0; i<dimA1; i++){
+    for(int j=0; j<dimB2; j++){
+      double sum = 0.0;
+      for(int k=0; k<dimA2B1;k++){
+	sum+=A[i*dimA2B1+k]*B[k*dimB2+j];
+      }
+      C[i*dimB2+j]=sum;
+    }
+  }
+  return C;
+
+}
